ca28959944b2dcfe047aa9779c1b02580cdbed8e
edgar <legamejiav@gmail.com>
2025-06-12 01:12:37 -0500

    build: add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,29 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import * as dotenv from 'dotenv';
-import { ConfigModule } from '@nestjs/config';
-
-dotenv.config();
-
-console.log(process.env.MONGO_URI);
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Importa ConfigService
+import * as path from 'node:path'; // Importa path
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI as string),
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que ConfigService esté disponible globalmente
+      envFilePath: path.resolve(
+        __dirname,
+        `../.env.${process.env.NODE_ENV || 'development'}`,
+      ),
+      // Si NODE_ENV no está definido, por defecto buscará '.env.development'
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // Importa ConfigModule para que ConfigService esté disponible
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'), // Obtiene MONGO_URI de ConfigService
+        // Puedes añadir más opciones de Mongoose aquí si las necesitas
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+      }),
+      inject: [ConfigService], // Indica que ConfigService debe ser inyectado
+    }),
     TasksModule,
     UsersModule,
     AuthModule,
